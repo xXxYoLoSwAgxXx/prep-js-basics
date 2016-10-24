f007be60a0e710ef56cd756a91cd469febfbeaa5
xXxYoLoSwAgxXx <harrisfambam@gmail.com>
2016-10-24 12:29:05 -1000

    prep js

--- index.js
+++ index.js
@@ -10,7 +10,9 @@
 * It should be a String.
 **/
 
-
+var firstName = "yolo";
+var lastName = "harrisboii";
+var birthPlace = "Hawaii";
 
 /**
 * #2
@@ -22,7 +24,9 @@
 * Their values should be a Number
 **/
 
-
+var favoriteNumber = 7;
+var currentYear = 2016;
+var thatOnePrinceSong = 1999;
 
 /**
 * #3
@@ -34,7 +38,10 @@
 * Their values should be a Boolean
 **/
 
-
+var isDaytime = true;
+var isLeftHanded = true;
+var inHawaii = true;
+var isHappyCoding = true;
 
 /**
 * #4
@@ -45,7 +52,8 @@
 * Their values should be a Null value
 **/
 
-
+var enrolledAtDevLeague = null;
+var completedFridayPrep = null;
 
 /**
 * #5a
@@ -56,7 +64,7 @@
 * which can be found along the west coast of The United States.
 **/
 
-
+var westCoast = ["Washington", "California", "Oregon", "Hawaii"];
 
 /**
 * #5b
@@ -67,7 +75,7 @@
 * Have at least ten even values in this array.
 **/
 
-
+var evenNumbers = [2, 4, 6, 8, 10, 12, 14, 16, 18, 20];
 
 /**
 * #6a Arithmetic
@@ -80,8 +88,7 @@
 * Extend this behavior to include subtraction, multiplication and division.
 */
 
-
-
+var sumOfNumbers = 1 + 2 + 3;
 
 /**
 * #6b Comparisons & Logic
@@ -89,17 +96,19 @@
 *
 * Declare a variable named `isEqual` which is a Boolean. This Boolean
 * contains the result of performing a comparison to test if two strings
-* are equal. We want to compare the two strings "Tacocat" and "tacocat".
+* are equal. We want to compare the two strings "Tacocat" and "tacocat" and
+* `console.log()` the result.
 *
 * NOTE:
-*   There are zero tests for the BONUS below.
+*   There is no test which tests your console.log()'s output'.
+*   Also, there are zero tests for the BONUS below.
 *
 * BONUS:
 *   See if you can come up with a few examples of double comparison
 *   operator(==) vs triple comparison operator(===).
 */
 
-
+var isEqual = "Tacocat" == "tacocat";
 
 /**
 * #6c Assignments
@@ -109,14 +118,27 @@
 * values as we assign them to show how variables in JavaScript are not bound to a single data type
 * and can change values.
 *
-* Write 5 different assignment statements.
+* Write 5 different assignment statements and `console.log()` the value of the variable after each
+* assignment statement to show the current value.
 *
 * NOTE:
 *   The tests for this section cannot test your variable as it changes,
 *   it can only test that you declared the variable by giving it the right name.
 */
 
+var generalContainer = "first";
 
+
+var generalContainer = "second";
+
+
+var generalContainer = "third";
+
+
+var generalContainer = "fourth";
+
+
+var generalContainer = "fifth";
 
 
 /**
@@ -128,10 +150,14 @@
 * This function should return a String, "You jumped 9 feet high!"
 * if invoked in this way -> jump(9);
 *
-* Store the return value to a variable named `jumpResult`
+* Store the return value to a variable named `jumpResult` and use console.log to inspect the value which was returned by your function
 **/
 
+function jump(height){
+  return "You jumped" + " " + height + " " + "feet high!";
+}
 
+jump(9)
 
 /**
 * #7b
@@ -143,3 +169,8 @@
 * if invoked in this way -> cook("Tomatoes", "Cheese", "Pizza");
 **/
 
+function cook(ingredient1, ingredient2, recipeName){
+  return(ingredient1 + " and " + ingredient2 + " make a " + recipeName + "!");
+}
+
+cook("Tomatoes", "Cheese", "Pizza")
